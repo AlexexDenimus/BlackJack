4eb61f0eff9ee042cc87fdd26a71ba94c87f9f99
AlexexDenimus <demin.leha1998@yandex.ru>
2020-02-06 13:24:02 +0300

    test3

--- deploy.sh
+++ deploy.sh
@@ -114,6 +114,7 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   echo "Running $NPM_CMD install --production"
   eval $NPM_CMD install --production
+  eval $NPM_CMD run rebuild
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
